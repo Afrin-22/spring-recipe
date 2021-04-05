932dfca43d6f3c9f7a55c472a3a65ce094548224
Afrin-22 <58210796+Afrin-22@users.noreply.github.com>
2021-04-05 12:22:28 +0530

    very redundent - so added helper method to make this simpler

--- src/main/java/com/springframework/recipe/domain/Recipe.java
+++ src/main/java/com/springframework/recipe/domain/Recipe.java
@@ -129,6 +129,12 @@ public class Recipe {
 	public void setNotes(Notes notes) {
 		this.notes = notes;
 	}
+	
+	 public Recipe addIngredient(Ingredient ingredient){
+	        ingredient.setRecipe(this);
+	        this.ingredients.add(ingredient);
+	        return this;
+	 }
 
 	public Set<Ingredient> getIngredients() {
 		return ingredients;
@@ -153,6 +159,5 @@ public class Recipe {
 	public void setCategories(Set<Category> categories) {
 		this.categories = categories;
 	}
-	
 	
 }
