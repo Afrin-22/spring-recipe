8963a596a01fe12a34f1d7ce8b16ec73284c15b3
Afrin-22 <58210796+Afrin-22@users.noreply.github.com>
2021-04-05 11:51:01 +0530

    added missed commits. need review!

---
+++ src/main/java/com/springframework/recipe/domain/Notes.java
@@ -0,0 +1,53 @@
+package com.springframework.recipe.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Notes {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	private Long id;
+	
+	@OneToOne
+	private Recipe recipe;
+	
+	@Lob
+	private String recipeNotes;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public Recipe getRecipe() {
+		return recipe;
+	}
+	public String getDescription() {
+		return recipeNotes;
+	}
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+	public void setDescription(String description) {
+		this.recipeNotes = description;
+	}
+
+	public String getRecipeNotes() {
+		return recipeNotes;
+	}
+
+	public void setRecipeNotes(String recipeNotes) {
+		this.recipeNotes = recipeNotes;
+	}
+	
+	
+}
